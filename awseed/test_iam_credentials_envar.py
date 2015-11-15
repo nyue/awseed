d39d922168a0918bce572049cd93844060a79b9a
Nicholas Yue <yue.nicholas@gmail.com>
2015-11-14 20:41:59 -0800

    Remove AWS credential (IAM user have already been deleted from AWS)

--- awseed/test_iam_credentials_envar.py
+++ awseed/test_iam_credentials_envar.py
@@ -2,8 +2,8 @@
 # AWS_SECRET_ACCESS_KEY - AWS secret key. Access and secret key variables override credentials stored in credential and config files.
 # AWS_DEFAULT_REGION - AWS region. This variable overrides the default region of the in-use profile, if set.
 #
-# env AWS_ACCESS_KEY_ID=AKIAJOVZ2DVGJKZAOJSQ \
-#     AWS_SECRET_ACCESS_KEY=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 \
+# env AWS_ACCESS_KEY_ID=acb \
+#     AWS_SECRET_ACCESS_KEY=xyz \
 #     AWS_DEFAULT_REGION=ap-southeast-2 \
 #     python test_iam_credentials_envar.py
 import boto3
